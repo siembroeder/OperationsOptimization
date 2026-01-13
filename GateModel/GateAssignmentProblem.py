7e7e9fa307a23a609652fc0d4b991170374bf7ab
siem broeder <siembroeder@gmail.com>
2026-01-13 15:52:07 +0100

    refactor: no more unnecessary list comprehension

--- GateModel/GateAssignmentProblem.py
+++ GateModel/GateAssignmentProblem.py
@@ -97,7 +97,7 @@ class GateAssignmentProblem:
         elif passenger_type == 'equal':
             self.p_ij = getTransferPassengers(self.all_aircraft, self.num_aircraft, self.all_aircraft_times)
             self.nt_i = {i: sum(self.p_ij[i][j] for j in self.all_aircraft) for i in self.all_aircraft}
-            self.e_i  = {i: np.random.randint(0,self.nt_i[i]) for i in self.all_aircraft}
+            self.e_i  = {i: np.random.randint(0,self.nt_i[i]) if self.nt_i[i] > 0 else 0 for i in self.all_aircraft}
             self.f_i  = {i: self.nt_i[i] - self.e_i[i] for i in self.all_aircraft}
 
         self.total_passengers = sum(self.nt_i[i] + sum(self.p_ij[i][j] for j in self.all_aircraft) for i in self.all_aircraft)
@@ -182,7 +182,7 @@ class GateAssignmentProblem:
             'model': model,
             'NA_star':self.NA_star,
             'total_pax':self.total_passengers,
-            'objective/pax': [objective/self.total_passengers if objective!=None else None][0]
+            'objective/pax': objective/self.total_passengers if objective is not None and self.total_passengers > 0 else 0
         }
 
     def plot_timetable(self, results, fig_save_path=None)-> None:
